17983c3421766f1731478603b6fedbe6c8c3e8a9
ZouHeng <zouheng_zhhe@163.com>
2017-08-17 11:47:42 +0800

    尝试Git

--- LearnCSharp/ConsoleApplication1/Program.cs
+++ LearnCSharp/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("尝试Git");
+            Console.ReadKey();
         }
     }
 }
